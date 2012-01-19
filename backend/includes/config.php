a528cecb00ba59e93310350e84dd6e5d130f0e55
Benjamin Gelhaar <bfgelhaar@googlemail.com>
2012-01-19 22:45:37 +0100

    added frontend folder

---
+++ backend/includes/config.php
@@ -0,0 +1,16 @@
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE);
+
+/*=========== Database Configuraiton ==========*/
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = 'root';
+$db_name = 'klipsmobile';
+
+
+/*=========== Website Configuration ==========*/
+
+
+?>
---
+++ backend/includes/views/event.php
@@ -0,0 +1,13 @@
+<?php 
+        
+        
+	//render($events,array('active'=>$_GET['event'])) 
+        
+    echo json_encode($events);
+    //echo var_dump($events);
+        
+        
+        
+?>
+
+
